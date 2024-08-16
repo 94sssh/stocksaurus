738ac8a2cf21a50a83df9eb560465e21739e5229
94sssh <abizsh@naver.com>
2024-08-17 00:18:10 +0900

    feat: 컴포넌트 전역 불러오기 설정

---
+++ src/theme/MDXComponents.tsx
@@ -0,0 +1,9 @@
+import MDXComponents from '@theme-original/MDXComponents';
+import StockCard from '@site/src/components/StockCard';
+import StockCardMini from '@site/src/components/StockCardMini';
+
+export default {
+  ...MDXComponents,
+  StockCard,
+  StockCardMini,
+};
