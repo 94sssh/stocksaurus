9a47c7398257591283b25255efb6899f404e4271
94sssh <abizsh@naver.com>
2024-08-17 00:17:30 +0900

    feat: StockCard 컴포넌트 추가

---
+++ src/components/StockCard.tsx
@@ -0,0 +1,28 @@
+import style from './StockCard.module.css';
+
+interface StockProps {
+  ticker: string;
+  price: number;
+  rate: number;
+}
+
+const StockCard = ({ ticker, price, rate }: StockProps) => {
+  return (
+    <a
+      href={`https://finance.yahoo.com/quote/${ticker}`}
+      className={style.stock}
+      target='_blank'
+      rel='noreferrer'
+    >
+      <span className={style.ticker}>{ticker}</span>
+      <div className={style.wrapper}>
+        <span>{price.toLocaleString()}</span>
+        <span className={style[rate >= 0 ? 'gainer' : 'loser']}>
+          {rate >= 0 ? '▲' : '▼'} {rate}%
+        </span>
+      </div>
+    </a>
+  );
+};
+
+export default StockCard;
