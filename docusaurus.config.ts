84c92e488146de50b3bfdc59e04ee221ef33890e
94sssh <abizsh@naver.com>
2024-11-20 10:17:08 +0900

    delete: 불필요한 blog 페이지 관련 파일 삭제

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -62,7 +62,7 @@ const config: Config = {
           position: 'left',
           label: 'News',
         },
-        { to: '/blog', label: 'Blog', position: 'left' },
+        // { to: '/blog', label: 'Blog', position: 'left' },
         {
           href: 'https://github.com/94sssh',
           label: 'GitHub',
