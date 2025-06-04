88b04cb395adfe93f038c683e6af60b3d2c47fa4
94sssh <abizsh@naver.com>
2025-06-05 00:19:30 +0900

    chore: 블로그 insight 페이지 세팅 추가

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,50 +1,68 @@
-import { themes as prismThemes } from "prism-react-renderer";
-import type { Config } from "@docusaurus/types";
-import type * as Preset from "@docusaurus/preset-classic";
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: "stocksaurus",
-  tagline: "미국 증시 요약",
-  favicon: "img/favicon.ico",
+  title: 'stocksaurus',
+  tagline: '미국 증시 요약',
+  favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: "https://stocksaurus.vercel.app/",
+  url: 'https://stocksaurus.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
 
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
   future: {
     experimental_faster: true,
   },
 
+  plugins: [
+    [
+      '@docusaurus/plugin-content-blog',
+      {
+        id: 'insight',
+        /**
+         * URL route for the blog section of your site.
+         * *DO NOT* include a trailing slash.
+         */
+        routeBasePath: 'insight',
+        /**
+         * Path to data on filesystem relative to site dir.
+         */
+        path: './insight',
+      },
+    ],
+  ],
+
   presets: [
     [
-      "classic",
+      '@docusaurus/preset-classic',
       {
         docs: {
-          sidebarPath: "./sidebars.ts",
+          sidebarPath: './sidebars.ts',
         },
         blog: {
           showReadingTime: false,
         },
         theme: {
-          customCss: "./src/css/custom.css",
+          customCss: './src/css/custom.css',
         },
       } satisfies Preset.Options,
     ],
@@ -52,58 +70,59 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/stocksaurus-social-card.png",
+    image: 'img/stocksaurus-social-card.png',
     navbar: {
-      title: "StockSaurus",
+      title: 'StockSaurus',
       logo: {
-        alt: "My Site Logo",
-        src: "img/stocksaurus.png",
+        alt: 'StockSaurus Logo',
+        src: 'img/stocksaurus.png',
       },
       items: [
         {
-          type: "docSidebar",
-          sidebarId: "newsSidebar",
-          position: "left",
-          label: "News",
+          type: 'docSidebar',
+          sidebarId: 'newsSidebar',
+          position: 'left',
+          label: 'News',
         },
-        { to: "/blog", label: "Blog", position: "left" },
+        { to: '/blog', label: 'Blog', position: 'left' },
+        { to: '/insight', label: 'Insight', position: 'left' },
       ],
     },
     footer: {
-      style: "dark",
+      style: 'dark',
       links: [
         {
-          title: "Docs",
+          title: 'Docs',
           items: [
             {
-              label: "News",
-              to: "/docs/intro",
+              label: 'News',
+              to: '/docs/intro',
             },
           ],
         },
         {
-          title: "Community",
+          title: 'Community',
           items: [
             {
-              label: "GitHub",
-              href: "https://github.com/94sssh",
+              label: 'GitHub',
+              href: 'https://github.com/94sssh',
             },
             {
-              label: "Blog",
-              href: "https://94sssh-code.vercel.app/",
+              label: 'Blog',
+              href: 'https://94sssh-code.vercel.app/',
             },
           ],
         },
         {
-          title: "More",
+          title: 'More',
           items: [
             {
-              label: "GitHub",
-              href: "https://github.com/94sssh",
+              label: 'GitHub',
+              href: 'https://github.com/94sssh',
             },
             {
-              label: "Blog",
-              to: "/blog",
+              label: 'Blog',
+              to: '/blog',
             },
           ],
         },
