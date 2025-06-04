37a13dc57120f5a48fd2064e251adb9b2607ef80
94sssh <abizsh@naver.com>
2025-06-05 08:07:10 +0900

    docu: 250604 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className='button button--secondary button--lg'
-            to='/docs/2025_06/3'
+            to='/docs/2025_06/4'
           >
             증시 요약 보기
           </Link>
